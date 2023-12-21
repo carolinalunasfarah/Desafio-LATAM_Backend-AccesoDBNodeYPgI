112b5866b10922382f61018599054dc853370605
Carolina Lunas <carolinalunasfarah@gmail.com>
2023-12-21 16:56:38 -0300

    create database, table and sql squema

---
+++ likeme-backend/database/squema.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE likeme;
+
+CREATE TABLE posts (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(25) NOT NULL,
+    img VARCHAR (1000) UNIQUE,
+    description VARCHAR(255) NOT NULL,
+    likes INT NOT NULL);
